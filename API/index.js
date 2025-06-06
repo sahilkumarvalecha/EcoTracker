8dc5c66b960c441c8a340da6a78ca500366a8f5e
SaleenaAhuja20 <saleenaahuja038@gmail.com>
2025-06-06 12:58:21 +0500

    admin sidebar and usermanagement

--- API/index.js
+++ API/index.js
@@ -375,6 +375,19 @@ app.get('/api/search-reports', async (req, res) => {
   }
 });
 
+// users fetch in admin panel
+// GET all users from the database
+app.get('/api/usersLoad', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT * FROM users');
+    res.json(result.rows);
+  } catch (err) {
+    console.error('Error fetching users:', err.message);
+    res.status(500).json({ error: 'Server error' });
+  }
+});
+
+
 
 
 // logout
